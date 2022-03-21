106c9914f7dfb3488f6f6c0ff6be4cf30c246a0b
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-21 13:54:23 -0700

    commit tree fd315dd3adecc539188493068ccd064cf07a3011

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,18 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  // const techList = technologies.map((tech) => {
+  //   return <span key={tech}>{tech}</span>;
+  // });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksList = links.map((link) => {
+    return (
+      <a key={link} href={`#${link}`}>
+        {link}
+      </a>
+    );
+  });
+  return <nav>{linksList}</nav>;
 }
 
 export default NavBar;
